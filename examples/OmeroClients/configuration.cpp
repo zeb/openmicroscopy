5720ac11b0924cd7007d2f1861fdf3f3ec12778e
jmoore <josh@glencoesoftware.com>
2011-02-20 12:31:44 +0000

    Fixing bad try/catch blocks in OmeroClients/configuration.cpp (See #4404)

--- examples/OmeroClients/configuration.cpp
+++ examples/OmeroClients/configuration.cpp
@@ -6,30 +6,46 @@ int main(int argc, char* argv[]) {
     // All configuration in file pointed to by
     // --Ice.Config=file.config
     // No username, password entered
-    omero::client client11(argc, argv);
-    client1.createSession();
-    client1.closeSession();
+    try {
+        omero::client client1(argc, argv);
+        client1.createSession();
+        client1.closeSession();
+    } catch (const Ice::ConnectionRefusedException& cre) {
+        // Bad address or port?
+    }
 
     // Most basic configuration.
     // Uses default port 4063
     // createSession needs username and password
-    omero::client client2("localhost");
-    client2.createSession("root", "ome");
-    client2.closeSession();
+    try {
+        omero::client client2("localhost");
+        client2.createSession("root", "ome");
+        client2.closeSession();
+    } catch (const Ice::ConnectionRefusedException& cre) {
+        // Bad address or port?
+    }
 
     // Configuration with port information
-    omero::client client3("localhost", 24063);
-    client3.createSession("root", "ome");
-    client3.closeSession();
+    try {
+        omero::client client3("localhost", 24063);
+        client3.createSession("root", "ome");
+        client3.closeSession();
+    } catch (const Ice::ConnectionRefusedException& cre) {
+        // Bad address or port?
+    }
 
     // Advanced configuration in C++ takes place
     // via an InitializationData instance.
-    Ice::InitializationData data;
-    data.properties = Ice::createProperties();
-    data.properties->setProperty("omero.host", "localhost");
-    omero::client client4(data);
-    client4.createSession("root", "ome");
-    client4.closeSession();
+    try {
+        Ice::InitializationData data;
+        data.properties = Ice::createProperties();
+        data.properties->setProperty("omero.host", "localhost");
+        omero::client client4(data);
+        client4.createSession("root", "ome");
+        client4.closeSession();
+    } catch (const Ice::ConnectionRefusedException& cre) {
+        // Bad address or port?
+    }
 
     // std::map to be added (ticket:1278)
     data.properties->setProperty("omero.user", "root");
@@ -38,6 +54,10 @@ int main(int argc, char* argv[]) {
     // Again, no username or password needed
     // since present in the data. But they *can*
     // be overridden.
-    client5.createSession();
-    client5.closeSession();
+    try {
+        client5.createSession();
+        client5.closeSession();
+    } catch (const Ice::ConnectionRefusedException& cre) {
+        // Bad address or port?
+    }
 }
